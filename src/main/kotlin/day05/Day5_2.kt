6bd2a4c3fbfbe51aca16f059dd85b90db9bcbb6b
Nohus <git@nohus.eu>
2024-12-05 05:55:14 +0000

    Added swap

--- src/main/kotlin/day05/Day5_2.kt
+++ src/main/kotlin/day05/Day5_2.kt
@@ -2,28 +2,20 @@ package day05
 
 import solveRaw
 import utils.mapToInts
+import utils.swap
 
 fun main() = solveRaw { input ->
     val (r, u) = input.split("\n\n").map { it.lines() }
     val rules = r.map { it.split("|").mapToInts() }
     val updates = u.map { it.split(",").mapToInts() }
 
-    fun isValid(update: List<Int>): Boolean {
-        return rules.filter { it.all { it in update } }.all { (a, b) ->
-            update.indexOf(a) < update.indexOf(b)
-        }
-    }
-
-    updates.filterNot { isValid(it) }.sumOf { update ->
+    updates.filterNot { update ->
+        rules.filter { it.all { it in update } }.all { (a, b) -> update.indexOf(a) < update.indexOf(b) }
+    }.sumOf { update ->
         val new = update.toMutableList()
-        for (i in new.indices) {
+        for (i in 0..(new.size / 2)) {
             for (j in i..new.lastIndex) {
-                val a = new[i]
-                val b = new[j]
-                if (rules.any { (first, second) -> first == b && second == a }) {
-                    new[i] = b
-                    new[j] = a
-                }
+                if (rules.any { (first, second) -> first == new[j] && second == new[i] }) new.swap(i, j)
             }
         }
         new[new.size / 2]
--- src/main/kotlin/utils/Parsing.kt
+++ src/main/kotlin/utils/Parsing.kt
@@ -12,23 +12,6 @@ fun String.toLongs(): List<Long> {
     return toWords().map { it.toLong() }
 }
 
-fun List<String>.mapToInts(): List<Int> {
-    return map { it.toInt() }
-}
-
-fun List<String>.mapToLongs(): List<Long> {
-    return map { it.toLong() }
-}
-
-fun <T> List<T>.toPair(): Pair<T, T> {
-    return this[0] to this[1]
-}
-
-fun <T> List<T>.withoutIndex(index: Int): List<T> {
-    require(index >= 0) { "Index must be positive" }
-    return take(index) + drop(index + 1)
-}
-
 fun MatchResult.getInts(): List<Int> {
     return groupValues.drop(1).mapToInts()
 }
